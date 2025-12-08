523a2a64a4ec0de1ff3cd9167db64fb775f59097
MSanan791 <sanan791@gmail.com>
2025-12-09 04:05:23 +0500

    minor bug fixes

--- ssd-data-collector/app/_layout.tsx
+++ ssd-data-collector/app/_layout.tsx
@@ -1,20 +1,23 @@
 import { Stack } from 'expo-router';
 import '../global.css';
 import { AuthProvider } from '../context/AuthContext';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'; // 1. Import this
 
 export default function RootLayout() {
   return (
-    <AuthProvider>
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
-      <Stack.Screen name="home" options={{ headerShown: false }} />
-      <Stack.Screen name="session-setup" options={{ headerShown: false }} />
-      <Stack.Screen name="recording" options={{ headerShown: false }} />
-      <Stack.Screen name="review-upload" options={{ headerShown: false }} />
-      <Stack.Screen name="patient-profile" options={{ headerShown: false }} />
-    </Stack>
-
-    </AuthProvider>
+    // 2. Wrap the entire app structure
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AuthProvider>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="home" options={{ headerShown: false }} />
+          <Stack.Screen name="session-setup" options={{ headerShown: false }} />
+          <Stack.Screen name="recording" options={{ headerShown: false }} />
+          <Stack.Screen name="review-upload" options={{ headerShown: false }} />
+          <Stack.Screen name="patient-profile" options={{ headerShown: false }} />
+          <Stack.Screen name="add-new-patient" options={{ headerShown: false }} />
+        </Stack>
+      </AuthProvider>
+    </GestureHandlerRootView>
   );
 }
